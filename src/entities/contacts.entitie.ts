fa51160ef0f2bc5d1dd8b16d765a7698b9a42c8d
Gabriel Carriel <ga123carriel2015@gmail.com>
2023-03-24 00:36:06 -0300

    feat: added contacts entity

---
+++ src/entities/contacts.entitie.ts
@@ -0,0 +1,31 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    ManyToOne
+} from "typeorm";
+import User from "./users.entitie";
+
+@Entity("contacts")
+class Contact {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column({ length: 60 })
+    name: string;
+
+    @Column({ length: 60, unique: true })
+    email: string;
+
+    @Column()
+    phone: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @ManyToOne(() => User)
+    user: User
+}
+
+export default Contact;
---
+++ src/migrations/1679627423556-createContact.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class createContact1679627423556 implements MigrationInterface {
+    name = 'createContact1679627423556'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "contacts" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(60) NOT NULL, "email" character varying(60) NOT NULL, "phone" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "userId" uuid, CONSTRAINT "UQ_752866c5247ddd34fd05559537d" UNIQUE ("email"), CONSTRAINT "PK_b99cd40cfd66a99f1571f4f72e6" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "contacts" ADD CONSTRAINT "FK_30ef77942fc8c05fcb829dcc61d" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "contacts" DROP CONSTRAINT "FK_30ef77942fc8c05fcb829dcc61d"`);
+        await queryRunner.query(`DROP TABLE "contacts"`);
+    }
+
+}
